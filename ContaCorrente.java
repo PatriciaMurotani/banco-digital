d523c7569a62124c93a577f209524b4b78688f31
Patricia Murotani <patricia.murotani@gmail.com>
2022-09-20 21:35:00 -0300

    projeto banco digital

---
+++ ContaCorrente.java
@@ -0,0 +1,15 @@
+public class ContaCorrente {
+    private int agencia;
+    private int conta;
+    private double saldo;
+
+    public void sacar() {
+    }
+
+    public void depositar(){
+    }
+
+    public void transferir() {
+    }
+
+}
---
+++ ContaPoupanca.java
@@ -0,0 +1,15 @@
+public class ContaPoupanca {
+    private int agencia;
+    private int conta;
+    private double saldo;
+
+    public void sacar() {
+    }
+
+    public void depositar(){
+    }
+
+    public void transferir() {
+    }
+
+}
